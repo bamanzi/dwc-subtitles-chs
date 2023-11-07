9c2849336444138b6a4a695fcb7a1784135999cc
Ba Manzi <ba.manzi@yahoo.com>
2023-11-07 16:56:05 +0800

    add package sdcv

--- tools/init.el
+++ tools/init.el
@@ -14,13 +14,16 @@
   ;; for `bing-dict
   (load-file (concat cwd "bing-dict.el"))
   (define-key search-map (kbd "M-b") 'bing-dict-brief)
+
+  (load-library "sdcv")
+  (define-key search-map (kbd "M-d") 'sdcv-search-pointer+)
   
   (defun subtitle-mode-my-init ()
     (setq fill-column 50)
+    (setq scroll-margin 0)
     (when (require 'fill-column-indicator nil t)
       (fci-mode 1)))
   (add-hook 'subtitle-mode-hook 'subtitle-mode-my-init)
-
 
  (defun srt-fix-chinese-punctuations ()
     (interactive)
